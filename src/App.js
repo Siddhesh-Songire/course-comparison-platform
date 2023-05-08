f47d0780ace6cf9f4eb2f3ce8969f1ab9b7b9ee5
Siddhesh <siddhehsongire@gmail.com>
2023-05-08 23:27:56 +0530

    Deleting some old trialed stuff

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import "./App.css";
 import React from "react";
 
 import Food from "./components/Food";
-import Horizontal_scroll from "./components/Horizontal_scroll";
 import Footer from "./components/Footer";
-import { EmblaCarousel } from "./components/EmblaCarousel";
 import Cards from "./components/Cards";
 import NavbarTaiwind from "./components/NavbarTaiwind";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Cards from "../components/Cards";
-import Horizontal_scroll from "../components/Horizontal_scroll";
 import About from "../components/About";
 
 const Home = () => {
